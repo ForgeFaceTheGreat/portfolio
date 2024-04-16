41a44a29da71c55893b94c06facb61d2f445e323
DESKTOP-IUJUGRQ\Forge Face The Great <jcway324@gmail.com>
2024-04-16 14:12:16 -0400

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
+import "./Navbar.css"
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import { Route, Routes } from 'react-router-dom'
 
-function App() {
+function App()
+{
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/About' element={<About />} />
+            <Route path='/Projects' element={<Projects />} />
+          </Routes>
+      </div>
+    </>
+  )
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+import { Link, useMatch, useResolvedPath } from 'react-router-dom'
+
+export default function Navbar()
+{
+    return <nav className="nav">
+        <Link to="/" className="site-title">Portfolio</Link>
+        <ul>
+            <CustomLink to="/home">Home</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
+            <CustomLink to="/projects">Projects</CustomLink>
+        </ul>
+    </nav>
+}
+
+function CustomLink({ to, children, ...props })
+{
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </li>
+    )
+}
