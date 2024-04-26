d2c2bd54a4af97f7cef8d001910b08ad9aa9eafa
DESKTOP-IUJUGRQ\Forge Face The Great <jcway324@gmail.com>
2024-04-26 15:51:43 -0400

    Update routing and 404 error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Error404 from './pages/404_error';
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 
 function App()
 {
@@ -15,11 +15,14 @@ function App()
       {/* Page Content */}
       <div className='container'>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/home' element={<Home />} />
+            {/* <Route path='/' element={<Home />} /> */}
+            <Route path='/' element={<Navigate replace to={'/home'} />} />
+            <Route path='/Home' element={<Home />} />
             <Route path='/About' element={<About />} />
             <Route path='/Projects' element={<Projects />} />
-            <Route path='*' element={<Error404 />} />
+
+            <Route path='/404_error' element={<Error404 />} />
+            <Route path='*' element={<Navigate replace to={'/404_error'} />} />
           </Routes>
       </div>
     </>
